85895bedcdfaa6b5bb3c57fe4391fd8fb2e5df78
Bang Nguyen <luongbang152@gmail.com>
2022-05-03 22:44:36 +0700

    update server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3456;
+
+app.use(cors());
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+
+  console.log("====== Webhook Received Start ===========");
+  console.log("------ Headers ---------");
+  console.log(req.headers);
+  console.log("------ Body ------");
+  console.log(req.body);
+  console.log("====== Webhook Received End ===========");
+  res.send("OK")
+});
+
+app.post('/webhook-failed', (req, res) => {
+  // We will be coding here
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
